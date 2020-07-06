308d26aad34236fad9a3b6d6fe8aa84dbec3b4ae
usmangta <usmangta@gmail.com>
2020-07-06 06:50:08 +0000

    added updated code for task 1

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -2,6 +2,7 @@
 # The goal of this task is to extract "hbtn"
 # from the source text, which in this case, is
 # being fed by argv/terminal.
-# This code handles and finds hbtn regardless of
-# how many 't' characters are present in the text
-puts ARGV[0].scan(/hbt*n/).join
+# This code finds and returns strings with
+# the pattern hbt*n only if t appears between
+# 2-5 times in the word
+puts ARGV[0].scan(/hbt{2,5}n/).join

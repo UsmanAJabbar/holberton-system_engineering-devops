6af4ab74bccef3c08eee44b24f2c9e95b7f5997c
UsmanGTA <usmangta@gmail.com>
2020-08-05 21:50:22 +0000

    added initial commit for task 7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,29 @@
+# Sets up a server based off of the tasks completed in tasks 1-4
+# with a single puppet file
+# REQUIREMENTS
+#	Nginx should be listening on port 80
+#	When querying Nginx at its root / with a GET request 
+#		(requesting a page) using curl, it must return a page that
+#		contains the string Holberton School
+#	The redirection must be a "301 Moved Permanently"
+include stdlib
+package { 'nginx':
+	ensure   => 'installed',
+	provider => 'apt',
+}
+
+file { '/var/www/html/index.html':
+	ensure  => file,
+	content => 'Holberton School',
+}
+
+file_line { '/etc/nginx/sites-available/default'
+	line     => "rewrite ^/redirect_me https://usmanjabbar.com permanent;",
+	ensure   => present,
+	multiple => true,
+	after    => "server_name _;",
+}
+
+service { 'nginx':
+	ensure => running,
+}

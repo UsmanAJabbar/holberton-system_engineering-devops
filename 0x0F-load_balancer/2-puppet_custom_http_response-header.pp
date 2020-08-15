ee5d94369306b134dc05b9602809fc7924673303
UsmanGTA <usmangta@gmail.com>
2020-08-15 04:47:08 +0000

    added

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -6,5 +6,5 @@ package {'haproxy':
 
 exec {'wget':
     provider => 'shell',
-    command  => 'wget https://raw.githubusercontent.com/GabrielAbdul/holberton-system_engineering-devops/master/0x0F-load_balancer/1-install_load_balancer && chmod +x 1-install_load_balancer && sudo ./1-install_load_balancer',
+    command  => 'wget https://raw.githubusercontent.com/UsmanGTA/holberton-system_engineering-devops/master/0x0F-load_balancer/1-install_load_balancer && chmod +x 1-install_load_balancer && sudo ./1-install_load_balancer',
 }

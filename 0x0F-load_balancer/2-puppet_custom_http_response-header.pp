a9e20e9ed8e997c88d2855429e55063c873ede33
UsmanGTA <usmangta@gmail.com>
2020-08-15 04:44:47 +0000

    added

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,9 +1,10 @@
-# This configures a custom NGINX server with
+# Puppet code to install NGINX
 package {'haproxy':
     ensure  => installed,
     version => '1.6.3',
 }
+
 exec {'wget':
     provider => 'shell',
-    command  => 'wget https://raw.githubusercontent.com/UsmanGTA/holberton-system_engineering-devops/master/0x0F-load_balancer/1-install_load_balancer && chmod +x 1-install_load_balancer && sudo ./1-install_load_balancer',
+    command  => 'wget https://raw.githubusercontent.com/GabrielAbdul/holberton-system_engineering-devops/master/0x0F-load_balancer/1-install_load_balancer && chmod +x 1-install_load_balancer && sudo ./1-install_load_balancer',
 }

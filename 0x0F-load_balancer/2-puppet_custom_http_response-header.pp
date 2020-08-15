4feaab63a9ee4b15ae5ed06694bd424d8307e6ff
UsmanGTA <usmangta@gmail.com>
2020-08-15 03:59:16 +0000

    added

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,17 @@
+# This configures a custom NGINX server with
+# basic files based off the tasks from 0 and 1
+package { 'nginx':
+    ensure   => 'installed',
+    provider => 'apt'
+}
+package { 'index.html':
+    ensure   => file,
+    path	 => '/var/www/html/index.html',
+    content  => 'Holberton School'
+}
+exec { 'Beep Beep Coming through':
+    command  => "sed -i '14a \\tadd_header X-Served-By $hostname;' /etc/nginx/sites-available/default"
+}
+service { 'nginx':
+	ensure   => running
+}

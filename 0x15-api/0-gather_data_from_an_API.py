53046a7fe6711ca8d2723088f56af9ac0a17a33e
UsmanGTA <usmangta@gmail.com>
2020-09-14 21:11:51 +0000

    moved them all to under a single nameguard

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,46 +5,30 @@ from sys import argv
 id, req = int(argv[1]), "https://jsonplaceholder.typicode.com"
 
 
-def name():
-    """Fetches the employee name from the JSON API"""
-    user_data = get(req + '/users').json()
-    for data in user_data:
-        if data['id'] == id:
-            specific_employee = data.get('name')
-            return specific_employee
-
-
-def comp_tasks():
-    """Fetches the number of tasks the specific employee has completed"""
-    task_data = get(req + '/todos').json()
-    completed_tasks = 0
-    for data in task_data:
-        if data['userId'] == id and data['completed'] is True:
-                completed_tasks += 1
-    return completed_tasks
-
-
-def total_tasks():
-    """Fetches the total number of tasks assigned to a specific employee"""
-    task_data = get(req + '/todos').json()
-    total_tasks = 0
-    for data in task_data:
-        if data['userId'] == id:
-                total_tasks += 1
-    return total_tasks
-
-
-def comp():
-    """Fetches the tasks the specific employee has completed"""
+if __name__ == "__main__":
     task_data = get(req + '/todos').json()
     completed_tasks = []
     for data in task_data:
         if data['userId'] == id and data['completed'] is True:
                 completed_tasks.append(data.get('title'))
-    return completed_tasks
 
-if __name__ == "__main__":
-    name, comp, total, task_list = name(), comp_tasks(), total_tasks(), comp()
-    print("Employee {} is done with tasks({}/{}):".format(name, comp, total))
-    for tasks in task_list:
+    task_data = get(req + '/todos').json()
+    total_tasks = 0
+    for data in task_data:
+        if data['userId'] == id:
+                total_tasks += 1
+
+    task_data = get(req + '/todos').json()
+    comp = 0
+    for data in task_data:
+        if data['userId'] == id and data['completed'] is True:
+                comp += 1
+
+    user_data = get(req + '/users').json()
+    for data in user_data:
+        if data['id'] == id:
+            name = data.get('name')
+
+    print("Employee {} is done with tasks({}/{}):".format(name, comp, total_tasks))
+    for tasks in completed_tasks:
         print('\t {}'.format(tasks))

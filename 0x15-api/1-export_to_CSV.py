b95b0221fcca4c4d8d6fe1c2bcf5499579fcea7e
UsmanGTA <usmangta@gmail.com>
2020-09-15 00:56:13 +0000

    added initial commit for task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Pulls the necessary data... and thank you for reading this"""
+import csv
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    id, req = int(argv[1]), "https://jsonplaceholder.typicode.com"
+
+    # GETS THE TITLE OF COMPLETES TASKS
+    task_data = get(req + '/todos').json()
+    completed_tasks = []
+    for data in task_data:
+        if data['userId'] == id and data['completed'] is True:
+                completed_tasks.append(data.get('title'))
+
+
+    # GETS THE NAME OF THE USER
+    user_data = get(req + '/users').json()
+    for data in user_data:
+        if data['id'] == id:
+            name = data.get('name')
+
+    filename = '{}.csv'.format(id)
+    with open(filename, mode='w') as file:
+        w = csv.writer(file, quotechar='"', quoting=csv.QUOTE_ALL)
+        for task in task_data:
+            w.writerow([id, name, task.get('completed'), task.get('title')])

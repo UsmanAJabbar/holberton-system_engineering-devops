2516ebf487273b1c007ec0c47ae14df0ec259454
UsmanGTA <usmangta@gmail.com>
2020-09-17 12:35:53 +0000

    added fixes for task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Returns a list of titles... this time, recursively"""
+
+
+def recurse(subreddit, hot_list=[], next=""):
+    """
+    ---------------
+    METHOD: recurse
+    ---------------
+    DESCRIPTION:
+        Returns a list of titles from a
+        specific subreddit.
+    Args:
+        @subreddit: subreddit to ping
+    Notes:
+        If the subreddit is invalid, this function
+        returns None, else, all of the post titles.
+    """
+    from requests import get as r_get
+
+    # Shortening names since PEP8's crap
+    sub, hot = subreddit, hot_list
+
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(sub,next)
+    data = r_get(url, allow_redirects=False, headers={'User-agent': ''}).json()
+    out = ([title['data']['title'] for title in data['data']['children'][:]]
+           if data.get('data') and data['data'].get('children')
+           is not None else None)
+
+    if out is not None:
+        hot += out
+        next = data['data']['after']
+        return recurse(sub, hot, next) if next is not None else hot

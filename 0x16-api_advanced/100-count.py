3dabe6f38e34e7e0ad935582aaa61ad1908f4da5
UsmanGTA <usmangta@gmail.com>
2020-09-17 19:59:38 +0000

    added PEP8 fixes

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -2,7 +2,7 @@
 """Returns a list of titles... this time, recursively"""
 
 
-def count_words(sub, word_list, next="", keys={}, page=0):
+def count_words(sub, word_list, next="", keys={}):
     """
     -------------------
     METHOD: count_words
@@ -24,7 +24,8 @@ def count_words(sub, word_list, next="", keys={}, page=0):
            if data.get('data') and data['data'].get('children')
            is not None else None)
 
-    # Create a dictionary storing the amount of times a specifc keyword was found in out
+    # Create a dictionary storing the amount of times a
+    # specifc keyword was found in out
     for words in word_list:
         keys[words] = 0 if words not in keys else keys[words]
         for titles in out:
@@ -34,7 +35,7 @@ def count_words(sub, word_list, next="", keys={}, page=0):
     if out is not None:
         next = data['data']['after']
         if next is not None:
-            return count_words(sub, word_list, next, keys, page=page+1)
+            return count_words(sub, word_list, next, keys)
         else:
             if keys == {}:
                 print()

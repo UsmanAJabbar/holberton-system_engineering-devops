dab95798d62dfec8464b73cd5e18dbba6efdf7c5
UsmanGTA <usmangta@gmail.com>
2020-09-30 23:13:28 +0000

    did it in the puppety way

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fixes the PHP extension in the wp-settings directory
 exec { 'phpextension':
-   provider => 'shell',
-   command =>  'wget https://raw.githubusercontent.com/UsmanGTA/holberton-system_engineering-devops/master/0x17-web_stack_debugging_3/wp_fixer.sh && chmod u+x wp_fixer.sh && sudo ./wp_fixer.sh',
+  path    => [ '/bin/', '/usr/bin/' ],
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
 }

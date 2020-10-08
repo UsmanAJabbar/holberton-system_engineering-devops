592edc96780f90cf7b2af43b731065e078ad757c
UsmanGTA <usmangta@gmail.com>
2020-10-08 15:07:27 +0000

    puppetlint

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,9 @@
 # Increases the number of files holberton as a user could handle
 exec { 'fix_hardlimit':
-	path    => ['/usr/bin/', '/bin/'],
-	command => 'sed -i "56c holberton hard nofile 1048576" /etc/security/limits.conf',
+  path    => ['/usr/bin/', '/bin/'],
+  command => 'sed -i "56c holberton hard nofile 1048576" /etc/security/limits.conf',
 }
 exec { 'fix_softlimit':
-	path    => ['/usr/bin/', '/bin/'],
-	command => 'sed -i "57c holberton soft nofile 1048576" /etc/security/limits.conf',
+  path    => ['/usr/bin/', '/bin/'],
+  command => 'sed -i "57c holberton soft nofile 1048576" /etc/security/limits.conf',
 }

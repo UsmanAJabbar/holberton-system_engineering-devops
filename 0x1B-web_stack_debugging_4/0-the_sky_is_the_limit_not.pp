14d1aaf097688809ee5fce128628b64d921c4579
UsmanGTA <usmangta@gmail.com>
2020-10-08 13:40:41 +0000

    added puppet fixes

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # Increases the number of requests nginx could handle
 exec { 'fix':
-	path    => ['/usr/bin/', '/bin/'],
-	command => 'echo ULIMIT=\"-n 4096\" >> /etc/default/nginx',
+  path    => ['/usr/bin/', '/bin/'],
+  command => 'echo ULIMIT=\"-n 4096\" >> /etc/default/nginx',
 }
 exec { 'restart service':
-	provider => 'shell',
-	command  => 'sudo service nginx restart',
+  provider => 'shell',
+  command  => 'sudo service nginx restart',
 }

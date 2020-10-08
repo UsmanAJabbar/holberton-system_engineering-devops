3fba305eaef6856c6b56ef030ad5e6c749403ea9
UsmanGTA <usmangta@gmail.com>
2020-10-08 13:38:24 +0000

    added

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # Increases the number of requests nginx could handle
 exec { 'fix':
 	path    => ['/usr/bin/', '/bin/'],
-	command => 'echo ULIMIT="-n 4096" >> /etc/default/nginx',
+	command => 'echo ULIMIT=\"-n 4096\" >> /etc/default/nginx',
 }
-exec { 'restart':
+exec { 'restart service':
 	provider => 'shell',
 	command  => 'sudo service nginx restart',
 }
